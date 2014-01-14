17828e8bac0b9cbe966c83574200413519646fa0
Braxton Huggins <braxton@delorum.com>
2014-01-14 16:11:53 -0700

    add test folder

---
+++ test/run-test.c
@@ -0,0 +1,13 @@
+// -*- mode: c; tab-width: 4; indent-tabs-mode: 1; st-rulers: [70] -*-
+// vim: ts=8 sw=8 ft=c noet
+
+#include <stdio.h>
+#include "bframe.h"
+
+int
+main(int argc, char **argv)
+{
+  printf("Testing libbframe\n");
+
+  return 0;
+}
