7ef0aa28fbb89117db82a668ba5db8f3a1cf1b81
Braxton Huggins <braxton@delorum.com>
2014-01-31 17:53:40 -0700

    update some things

--- src/bframe.c
+++ src/bframe.c
@@ -1,6 +1,7 @@
 // -*- mode: c; tab-width: 4; indent-tabs-mode: 1; st-rulers: [70] -*-
 // vim: ts=8 sw=8 ft=c noet
 
+#include <arpa/inet.h>
 #include "bframe.h"
 
 bframe_buffer_t 
@@ -29,11 +30,11 @@ count_bframes(char *data, int data_len)
 {
 	int count = 0;
 	int offset = 0;
-	bframe_len_t *bframe_len;
+	bframe_len_t bframe_len;
 	do {
 		count++;
-		bframe_len = (bframe_len_t *)(data + offset);
-		offset += bframe_len->int_len + 4;
+		bframe_len.int_len = ntohl(*((uint32_t *)(data + offset)));
+		offset += bframe_len.int_len + 4;
 	} while (offset < data_len - 4);
 
 	if (offset > data_len)
@@ -46,6 +47,7 @@ bframe_t
 **parse_char_to_bframes(char *data, int data_len, 
 	bframe_buffer_t *bframe_buffer, int *number_of_frames)
 {
+	bframe_len_t bigend_len;
 	char *local_buffer = NULL;
 	int local_buffer_len = 0;
 	bframe_len_t bframe_len;
@@ -71,14 +73,15 @@ bframe_t
 		bframe_buffer->len = 0;
 	}
 	*number_of_frames = count_bframes(local_buffer, local_buffer_len);
-	bframes = (bframe_t **)malloc(sizeof(bframe_t) * (*number_of_frames));
+	bframes = (bframe_t **)malloc(sizeof(bframe_t *) * (*number_of_frames));
 	while (count < *number_of_frames) {
-		bframes[count]= new_bframe(local_buffer + offset + 4, *((int *) (local_buffer + offset)));
+		uint32_t len = htonl(*((uint32_t *) (local_buffer + offset)));
+		bframes[count]= new_bframe(local_buffer + offset + 4, len);
 		// memcpy(bframes[count].len.char_len, local_buffer + offset, 4);
 		// bframes[count].data = (char *)malloc(bframes[count].len.int_len + 1);
 		// bframes[count].data[bframes[count].len.int_len] = '\0';
 		// memcpy(bframes[count].data, local_buffer + offset + 4, bframes[count].len.int_len);
-		offset += bframes[count]->len.int_len + 4;
+		offset += len + 4;
 		count++;
 	}
 	if (offset < local_buffer_len) {
@@ -96,11 +99,12 @@ char
 *bframe_to_char(bframe_t *bframe, int *length)
 {
 	char *packed_frame;
-
+	bframe_len_t bigend_len;
+	bigend_len.int_len = htonl(bframe->len.int_len);
 	*length = bframe->len.int_len + 4;
 	packed_frame = (char *)malloc(sizeof(char) * (*length) + 1);
 	packed_frame[*length] = '\0';
-	memcpy(packed_frame, &(bframe->len.char_len), 4);
+	memcpy(packed_frame, &(bigend_len.char_len), 4);
 	memcpy(packed_frame + 4, bframe->data, bframe->len.int_len);
 	return packed_frame;
 }
